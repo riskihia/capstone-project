9496f9f1488bc0965a01394843d2c1a63921c27e
Riski Rahmat Hia <riskirahmathia3@gmail.com>
2023-06-04 21:48:33 +0700

    rdy dummy data ALL

--- util/dumy_data.py
+++ util/dumy_data.py
@@ -3,6 +3,7 @@ import uuid
 from util.config import db
 from model.models import PenggunaModel, LahanModel, LahanImageModel
 from sqlalchemy import inspect
+import random
 
 
 def has_data(model):
@@ -18,141 +19,84 @@ def CekTabel(nama_table):
 def populate_data():
     if CekTabel("pengguna") and not has_data(PenggunaModel):
         insert_pengguna()
-    if CekTabel("lahan") and not has_data(LahanModel):
-        insert_lahan()
     if CekTabel("lahan_image") and not has_data(LahanImageModel):
         insert_lahan_image()
 
     db.session.commit()
 
 
-def insert_pengguna():
-    user_id = []
-    for _ in range(5):
-        user_id.append(uuid.uuid4())
-    data_user = [
-        {
-            "id": user_id[0],
-            "username": "user1",
-            "email": "user1@gmail.com",
-            "photo": "https://storage.googleapis.com/flask-api-bucket/profile/default_profile.png",
-            "premium": False,
-            "terakhir_login": datetime.datetime.utcnow(),
-        },
-        {
-            "id": user_id[1],
-            "username": "user2",
-            "email": "user2@gmail.com",
-            "photo": "https://storage.googleapis.com/flask-api-bucket/profile/default_profile.png",
-            "premium": False,
-            "terakhir_login": datetime.datetime.utcnow(),
-        },
-        {
-            "id": user_id[2],
-            "username": "user3",
-            "email": "user3@gmail.com",
-            "photo": "https://storage.googleapis.com/flask-api-bucket/profile/default_profile.png",
-            "premium": False,
-            "terakhir_login": datetime.datetime.utcnow(),
-        },
-        {
-            "id": user_id[3],
-            "username": "user4",
-            "email": "user4@gmail.com",
-            "photo": "https://storage.googleapis.com/flask-api-bucket/profile/default_profile.png",
-            "premium": False,
-            "terakhir_login": datetime.datetime.utcnow(),
-        },
-        {
-            "id": user_id[4],
-            "username": "user5",
-            "email": "user5@gmail.com",
-            "photo": "https://storage.googleapis.com/flask-api-bucket/profile/default_profile.png",
-            "premium": False,
-            "terakhir_login": datetime.datetime.utcnow(),
-        },
+def get_photo_list():
+    list_photo = [
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/a1.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/a2.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/a3.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/a4.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/b1.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/b2.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/b3.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/b4.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/c1.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/c2.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/c3.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/c4.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/d1.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/d2.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/d3.png",
+        "https://storage.googleapis.com/flask-api-bucket/lahan_image/d4.png",
     ]
 
-    for item in data_user:
+    return list_photo
+
+
+def insert_pengguna():
+    user_data = []
+    for i in range(1, 6):
+        user = {
+            "username": f"user{i}",
+            "email": f"user{i}@gmail.com",
+        }
+        user_data.append(user)
+
+    for item in user_data:
+        user_id = uuid.uuid4()
         pengguna = PenggunaModel(
-            id=item["id"],
+            id=user_id,
             username=item["username"],
             email=item["email"],
-            photo=item["photo"],
-            premium=item["premium"],
-            terakhir_login=item["terakhir_login"],
+            photo="https://storage.googleapis.com/flask-api-bucket/profile/default_profile.png",
+            premium=False,
+            terakhir_login=db.func.now(),
+            updated_at=db.func.now(),
         )
         db.session.add(pengguna)
 
-        lahan_id = []
-        for _ in range(6):
-            lahan_id.append(uuid.uuid4())
+        lahan_data = []
+        num_iterations = random.randint(1, 10)  # Jumlah iterasi acak antara 1 dan 10
 
-        data_lahan = [
-            {
-                "id": lahan_id[0],
-                "user_id": item["id"],
-                "nama": "Lahan 1",
-                "luas": 100.0,
-                "alamat": "Alamat 1",
-                "lat": 0.0,
-                "lon": 0.0,
-            },
-            {
-                "id": lahan_id[1],
-                "user_id": item["id"],
-                "nama": "Lahan 2",
-                "luas": 200.0,
-                "alamat": "Alamat 2",
-                "lat": 1.1,
-                "lon": 1.1,
-            },
-            {
-                "id": lahan_id[2],
-                "user_id": item["id"],
-                "nama": "Lahan 3",
-                "luas": 300.0,
-                "alamat": "Alamat 3",
-                "lat": 3.3,
-                "lon": 3.3,
-            },
-            {
-                "id": lahan_id[3],
-                "user_id": item["id"],
-                "nama": "Lahan 4",
-                "luas": 400.0,
-                "alamat": "Alamat 4",
-                "lat": 4.4,
-                "lon": 4.4,
-            },
-            {
-                "id": lahan_id[4],
-                "user_id": item["id"],
-                "nama": "Lahan 5",
-                "luas": 500.0,
-                "alamat": "Alamat 5",
-                "lat": 5.5,
-                "lon": 5.5,
-            },
-            {
-                "id": lahan_id[5],
-                "user_id": item["id"],
-                "nama": "Lahan 6",
-                "luas": 600.0,
-                "alamat": "Alamat 6",
-                "lat": 6.6,
-                "lon": 6.6,
-            },
-        ]
-        for lahanx in data_lahan:
+        for i in range(1, num_iterations + 1):
+            photo = random.choice(get_photo_list())
+            lahan = {
+                "nama": f"Lahan {i}",
+                "luas": i * 100.0,
+                "alamat": f"Alamat {i}",
+                "photo": photo,
+                "lat": i * 1.1,
+                "lon": i * 1.1,
+            }
+            lahan_data.append(lahan)
+
+        for lahanx in lahan_data:
+            lahan_id = uuid.uuid4()
             lahan = LahanModel(
-                id=lahanx["id"],
-                user_id=lahanx["user_id"],
+                id=lahan_id,
+                user_id=user_id,
                 nama=lahanx["nama"],
+                photo=lahanx["photo"],
                 luas=lahanx["luas"],
                 alamat=lahanx["alamat"],
                 lat=lahanx["lat"],
                 lon=lahanx["lon"],
+                updated_at=db.func.now(),
             )
             db.session.add(lahan)
 
@@ -160,6 +104,12 @@ def insert_pengguna():
 
 
 def insert_lahan_image():
-    lahan_image = LahanImageModel(nama="Image 1", photo="image1.jpg")
-    db.session.add(lahan_image)
+    photos = get_photo_list()
+    for i, photo in enumerate(photos):
+        lahan_image = LahanImageModel(
+            nama=f"Image {i+1}",
+            photo=photo,
+            updated_at=db.func.now(),
+        )
+        db.session.add(lahan_image)
     db.session.commit()
